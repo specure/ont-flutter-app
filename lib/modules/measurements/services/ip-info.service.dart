ff63fc90498727290a9d02614b791146015aa999
Apple <apple@Jozefs-Mac-Studio.local>
2024-04-18 16:23:12 +0200

    Update libraries

--- lib/modules/measurements/services/ip-info.service.dart
+++ lib/modules/measurements/services/ip-info.service.dart
@@ -1,27 +1,23 @@
+import 'dart:convert';
 import 'dart:io';
-import 'package:dio/dio.dart';
-import 'package:get_it/get_it.dart';
 import 'package:nt_flutter_standalone/core/constants/environment.dart';
 import 'package:nt_flutter_standalone/core/constants/urls.dart';
 import 'package:nt_flutter_standalone/core/services/dio.service.dart';
-import 'package:nt_flutter_standalone/core/wrappers/internet-address.wrapper.dart';
 import 'package:nt_flutter_standalone/modules/measurements/models/network-info-details.dart';
 import 'package:nt_flutter_standalone/modules/measurements/models/server-network-types.dart';
+import 'package:http/http.dart' as http;
 
 class IPInfoService extends DioService {
   IPInfoService({bool testing = false}) : super(testing: testing);
 
   Future<String> getPublicAddress(IPVersion version) async {
     try {
-      final Dio localDio = Dio(dio.options);
-      localDio.options.baseUrl = 'https://' +
-          Environment.controlServerUrl
-              .replaceFirst('.net', 'v${version.toInt()}.net');
-      await GetIt.I
-          .get<InternetAddressWrapper>()
-          .lookup(localDio.options.baseUrl + NTUrls.csIpRoute);
-      final response = await localDio.post(NTUrls.csIpRoute);
-      return response.data['ip'];
+      final host = Environment.controlServerUrl
+          .replaceFirst('.net', 'v${version.toInt()}.net');
+      final response = await http
+          .post(Uri(scheme: "https", host: host, path: NTUrls.csIpRoute));
+      final parsed = jsonDecode(response.body) as Map;
+      return parsed['ip'];
     } catch (_) {
       return addressIsNotAvailable;
     }
